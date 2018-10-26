aed9089ea6f4e089c1ef8b38eea76a215975bd7f
Djordje Zivanovic <djordjezivanovicva@gmail.com>
2018-10-27 00:45:36 +0100

    1. Calucate y based on mean and plot using only bar functions.

--- test.py
+++ test.py
@@ -19,11 +19,21 @@ def split_data(X, y):
     return X_train, y_train, X_test, y_test
 
 
+def plot_bar_chart_score(X_train, y_train):
+    fix, ax = plt.subplots()
+    unique, counts = np.unique(y_train, return_counts=True)
+    print(counts)
+    plt.bar(unique, counts)
+    plt.show()
+
+
+def predict_simple(X_train, y_train):
+    return y_train.mean()
+
+
 if __name__ == "__main__":
     X, y = load_data()
     X_train, y_train, X_test, y_test = split_data(X, y)
-
-    n, bins, patches = plt.hist(y_train)
-    plt.show()
-
-m = 1
+    plot_bar_chart_score(X_train, y_train)
+    print("Score is {}".format(predict_simple(X_train, y_train)))
+    m = 1
