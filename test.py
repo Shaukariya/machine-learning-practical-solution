940318de6fbe318f847bb77c924c03b5bbdf95d6
Djordje Zivanovic <djordjezivanovicva@gmail.com>
2018-10-27 17:17:31 +0100

    1. Added mean squared error calculation.

--- test.py
+++ test.py
@@ -22,18 +22,25 @@ def split_data(X, y):
 def plot_bar_chart_score(X_train, y_train):
     fix, ax = plt.subplots()
     unique, counts = np.unique(y_train, return_counts=True)
-    print(counts)
     plt.bar(unique, counts)
     plt.show()
 
 
-def predict_simple(X_train, y_train):
-    return y_train.mean()
+def predict_simple(y_test):
+    return int(y_test.mean())
+
+
+def test_data(X_test, y_test, predictor: callable=None):
+    # Applies function over rows.
+    y_predicted = np.apply_along_axis(predictor, 1, X_test)
+    mse = np.mean(np.square(np.subtract(y_predicted, y_test)))
+    print("Mean squared error is {}".format(mse))
 
 
 if __name__ == "__main__":
     X, y = load_data()
     X_train, y_train, X_test, y_test = split_data(X, y)
     plot_bar_chart_score(X_train, y_train)
-    print("Score is {}".format(predict_simple(X_train, y_train)))
+    print("Score is {}".format(predict_simple(y_train)))
+    test_data(X_test, y_test, lambda x: predict_simple(y_train))
     m = 1
