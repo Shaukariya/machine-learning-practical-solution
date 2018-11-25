5f046db50a48fa10cafdecda0ca7f4ff95e88c80
Djordje Zivanovic <djordjezivanovicva@gmail.com>
2018-11-25 01:40:44 +0000

    1. Implemented CNN requested in practical.

--- practical3/test.py
+++ practical3/test.py
@@ -7,7 +7,7 @@ BATCH_SIZE = 50
 TEST_ID = 50
 
 
-def reshape_data(X, y, is_image: bool= False):
+def reshape_data(X, y):
     N = X.shape[0]
     X = np.reshape(X, (N, 28, 28, 1))
     y_oh = np.zeros((N, 10))
@@ -25,7 +25,11 @@ def bias_variable(shape):
     return tf.Variable(initial)
 
 
-def conv2d(x, W):
+def conv2dl1(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 2, 2, 1], padding='VALID')
+
+
+def conv2dl2(x, W):
     return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
 
@@ -34,9 +38,7 @@ def max_pool_2x2(x):
 
 
 if __name__ == "__main__":
-
-    mnist = tf.keras.datasets.mnist
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
     x_train, x_test = x_train / 255.0, x_test / 255.0
 
     '''
@@ -53,23 +55,21 @@ if __name__ == "__main__":
     y_ = tf.placeholder(tf.float32, shape=[None, 10])
 
     # Make the fully connected layer
-    W_conv1 = weight_variable([5, 5, 1, 32])
-    b_conv1 = bias_variable([32])
+    W_conv1 = weight_variable([12, 12, 1, 25])
+    b_conv1 = bias_variable([25])
     x_image = tf.reshape(x, [-1, 28, 28, 1])
-    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-    h_pool1 = max_pool_2x2(h_conv1)
+    h_conv1 = tf.nn.relu(conv2dl1(x_image, W_conv1) + b_conv1)
 
     # Make second fully connected layer
-    W_conv2 = weight_variable([5, 5, 32, 64])
+    W_conv2 = weight_variable([5, 5, 25, 64])
     b_conv2 = bias_variable([64])
+    h_conv2 = tf.nn.relu(conv2dl2(h_conv1, W_conv2) + b_conv2)
 
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
     h_pool2 = max_pool_2x2(h_conv2)
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 5 * 5 * 64])
 
-    W_fc1 = weight_variable([7 * 7 * 64, 1024])
+    W_fc1 = weight_variable([5 * 5 * 64, 1024])
     b_fc1 = bias_variable([1024])
-
-    h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
     h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
     keep_prob = tf.placeholder(tf.float32)
@@ -84,13 +84,12 @@ if __name__ == "__main__":
     train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
     correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
-    train_accs = np.zeros(50)
-    valid_accs = np.zeros(50)
+    train_accs = np.zeros(100)
+    valid_accs = np.zeros(100)
 
-    x_train, y_train = reshape_data(x_train, y_train, is_image=True)
-    x_test, y_test = reshape_data(x_test, y_test, is_image=True)
+    x_train, y_train = reshape_data(x_train, y_train)
+    x_test, y_test = reshape_data(x_test, y_test)
 
     with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
         sess.run(tf.global_variables_initializer())
